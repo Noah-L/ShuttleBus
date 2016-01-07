660bcd66bf6d2da58e417f9011f78a4ebb6d23cd
wury <ryan.wu@oocl.com>
2016-01-08 04:47:35 +0800

    save ticket update

--- src/com/example/shuttlebus/TicketActivity.java
+++ src/com/example/shuttlebus/TicketActivity.java
@@ -1,7 +1,7 @@
 package com.example.shuttlebus;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -9,8 +9,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.oocl.shuttlebus.common.DateHelper;
 import com.oocl.shuttlebus.common.SharePreferenceHelper;
-import com.oocl.shuttlebus.mockdata.MockData;
 import com.oocl.shuttlebus.model.Ticket;
 import com.oocl.shuttlebus.model.User;
 
@@ -21,8 +21,8 @@ public class TicketActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.ticket);
 		
-		List<Ticket> mockupDatas =  MockData.mockTicket();
-		SharePreferenceHelper.saveTickets(getApplicationContext(), mockupDatas);
+//		List<Ticket> mockupDatas =  MockData.mockTicket();
+//		SharePreferenceHelper.saveTickets(getApplicationContext(), mockupDatas);
 		
 		User user = SharePreferenceHelper.getUser(getApplicationContext());		
 		List<Ticket> tickets = SharePreferenceHelper.geTickets(getApplicationContext());
@@ -30,7 +30,7 @@ public class TicketActivity extends Activity {
 		List<Ticket> filterTickets = new ArrayList<Ticket>();
 		for(Ticket ticket : tickets){
 			Date date = new Date();
-			if(ticket.getUser().getId()== user.getId() && areSameDay(ticket.getDate(),date)){
+			if(ticket.getUser().getId()== user.getId() && DateHelper.areSameDay(ticket.getDate(),date)){
 				filterTickets.add(ticket);
 			}
 		}
@@ -42,28 +42,18 @@ public class TicketActivity extends Activity {
 		}
 	}
 	
-	public boolean areSameDay(Date dateA,Date dateB) {
-	    Calendar calDateA = Calendar.getInstance();
-	    calDateA.setTime(dateA);
-
-	    Calendar calDateB = Calendar.getInstance();
-	    calDateB.setTime(dateB);
-
-	    return calDateA.get(Calendar.YEAR) == calDateB.get(Calendar.YEAR)
-	            && calDateA.get(Calendar.MONTH) == calDateB.get(Calendar.MONTH)
-	            &&  calDateA.get(Calendar.DAY_OF_MONTH) == calDateB.get(Calendar.DAY_OF_MONTH);
-	}
-	
 	private void initTicketView(Ticket firstTicket, Ticket secondTicket){
 		TextView nameOfFirstTicket = (TextView)findViewById(R.id.nameOfFirstTicket);
 		TextView routeOfFirstTicket = (TextView)findViewById(R.id.routeOfFirstTicket);
 		TextView stopOfFirstTicket = (TextView)findViewById(R.id.stopOfFirstTicket);
 		TextView typeOfFirstTicket = (TextView)findViewById(R.id.typeOfFirstTicket);
 		
+		SimpleDateFormat format=new SimpleDateFormat("yyyy MM dd"); 
+		
 		nameOfFirstTicket.setText("姓名 : "+firstTicket.getUser().getUserName());
 		routeOfFirstTicket.setText("路线: "+firstTicket.getRoute().getName());
 		stopOfFirstTicket.setText("站点: "+firstTicket.getBusStop().getName());
-		typeOfFirstTicket.setText("类型: "+firstTicket.getType() +""+firstTicket.getDate());
+		typeOfFirstTicket.setText("类型: "+firstTicket.getType() +" "+format.format(firstTicket.getDate()));
 		
 		TextView nameOfSecondTicket = (TextView)findViewById(R.id.nameOfSecondTicket);
 		TextView routeOfSecondTicket = (TextView)findViewById(R.id.routeOfSecondTicket);
@@ -73,7 +63,7 @@ public class TicketActivity extends Activity {
 		nameOfSecondTicket.setText("姓名 : "+secondTicket.getUser().getUserName());
 		routeOfSecondTicket.setText("路线: "+secondTicket.getRoute().getName());
 		stopOfSecondTicket.setText("站点: "+secondTicket.getBusStop().getName());
-		typeOfSecondTicket.setText("类型: "+secondTicket.getType() +"" + secondTicket.getDate());
+		typeOfSecondTicket.setText("类型: "+secondTicket.getType() +" " + format.format(secondTicket.getDate()));
 	}
 
 
